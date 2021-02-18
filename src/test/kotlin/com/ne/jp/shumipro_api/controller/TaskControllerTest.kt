ba834834fadeb686555f48891f45726a54cdcee7
Tom-Shumi <tshumiya281@gmail.com>
2021-02-18 23:39:39 +0900

    TaskControllerのtestコード作成 　GET完了

--- src/test/kotlin/com/ne/jp/shumipro_api/controller/TaskControllerTest.kt
+++ src/test/kotlin/com/ne/jp/shumipro_api/controller/TaskControllerTest.kt
@@ -1,13 +1,18 @@
 package com.ne.jp.shumipro_api.controller
 
+import com.google.gson.Gson
 import com.ne.jp.shumipro_api.ShumiproApiApplication
+import com.ne.jp.shumipro_api.dto.TaskDto
 import com.ne.jp.shumipro_api.entity.ShumiproLoginUser
 import com.ne.jp.shumipro_api.entity.User
+import com.ne.jp.shumipro_api.response.TaskResponse
+import com.ne.jp.shumipro_api.service.TaskService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
@@ -18,23 +23,26 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.RequestBuilder
 
-
-
-
 @ExtendWith(SpringExtension::class)
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc()
 @SpringBootTest(classes = [ShumiproApiApplication::class])
 class TaskControllerTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
+    @MockkBean
+    lateinit var mockTaskService: TaskService
+
+    open val gson = Gson()
 
     @Test
     fun getTask_task1() {
         val user:User = User("shumiya", "shumiya", 1, 1)
         val loginUser = ShumiproLoginUser(user)
-        val inputJson = ""
-        val expectedJson = ""
+        val expectedTaskDtoList = listOf(TaskDto(1,"shumiya", "test", 1, 1))
+        val expected = listOf(TaskResponse(1,"shumiya","test",1, 1))
+        val expectedJson = gson.toJson(expected)
+        every { mockTaskService.getTaskList("shumiya") } returns expectedTaskDtoList
 
         val builder: RequestBuilder = MockMvcRequestBuilders.get("/api/task")
             .with(user(loginUser))
@@ -44,6 +52,5 @@ class TaskControllerTest {
         mockMvc.perform(builder)
             .andExpect(status().isOk)
             .andExpect(content().json(expectedJson))
-            .andReturn()
     }
 }
