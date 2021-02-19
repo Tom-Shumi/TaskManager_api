b190808f15be7ec9927fa41b8e8aa7fae3196082
Tom-Shumi <tshumiya281@gmail.com>
2021-02-19 23:10:41 +0900

    TaskControllerのtestコード作成 　post　途中

--- src/test/kotlin/com/ne/jp/shumipro_api/controller/TaskControllerTest.kt
+++ src/test/kotlin/com/ne/jp/shumipro_api/controller/TaskControllerTest.kt
@@ -5,16 +5,20 @@ import com.ne.jp.shumipro_api.ShumiproApiApplication
 import com.ne.jp.shumipro_api.dto.TaskDto
 import com.ne.jp.shumipro_api.entity.ShumiproLoginUser
 import com.ne.jp.shumipro_api.entity.User
+import com.ne.jp.shumipro_api.mapper.TaskMapper
+import com.ne.jp.shumipro_api.request.TaskRequest
 import com.ne.jp.shumipro_api.response.TaskResponse
 import com.ne.jp.shumipro_api.service.TaskService
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
@@ -30,19 +34,20 @@ class TaskControllerTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
-    @MockkBean
-    lateinit var mockTaskService: TaskService
+//    @MockkBean
+//    lateinit var mockTaskService: TaskService
+    @Autowired
+    private lateinit var taskMapper: TaskMapper
 
     open val gson = Gson()
 
     @Test
-    fun getTask_task1() {
-        val user:User = User("shumiya", "shumiya", 1, 1)
-        val loginUser = ShumiproLoginUser(user)
+    fun getTask_test1() {
+        val loginUser = createLoginUser()
         val expectedTaskDtoList = listOf(TaskDto(1,"shumiya", "test", 1, 1))
         val expected = listOf(TaskResponse(1,"shumiya","test",1, 1))
         val expectedJson = gson.toJson(expected)
-        every { mockTaskService.getTaskList("shumiya") } returns expectedTaskDtoList
+//        every { mockTaskService.getTaskList("shumiya") } returns expectedTaskDtoList
 
         val builder: RequestBuilder = MockMvcRequestBuilders.get("/api/task")
             .with(user(loginUser))
@@ -53,4 +58,31 @@ class TaskControllerTest {
             .andExpect(status().isOk)
             .andExpect(content().json(expectedJson))
     }
+
+    @Test
+    fun registerTask_task1(){
+        val input = TaskRequest("test",1, 1)
+        val inputJson = gson.toJson(input)
+
+        val loginUser = createLoginUser()
+        val builder: RequestBuilder = MockMvcRequestBuilders.post("/api/task")
+            .with(user(loginUser))
+            .with(csrf())
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(inputJson)
+
+        mockMvc.perform(builder)
+            .andExpect(status().isOk)
+
+        val taskList = taskMapper.getTaskByUsername("shumiya")
+        val targetTask = taskList?.get(0)
+        assertThat(targetTask?.username).isEqualTo("shumiya")
+        assertThat(targetTask?.task).isEqualTo("test")
+        assertThat(targetTask?.priority).isEqualTo(1)
+        assertThat(targetTask?.status).isEqualTo(1)
+
+    }
+
+    fun createLoginUser(): ShumiproLoginUser = ShumiproLoginUser(User("shumiya", "shumiya", 1, 1))
 }
