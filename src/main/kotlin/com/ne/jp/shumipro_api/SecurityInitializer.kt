bf3a9875d35a622551181c24c0c3c0af2ef2c734
Tom-Shumi <tshumiya281@gmail.com>
2021-05-15 11:19:33 +0900

    fix  SecurityInitializer

--- src/main/kotlin/com/ne/jp/shumipro_api/SecurityInitializer.kt
+++ src/main/kotlin/com/ne/jp/shumipro_api/SecurityInitializer.kt
@@ -1,9 +1,10 @@
 package com.ne.jp.shumipro_api
 
 import com.ne.jp.shumipro_api.config.SessionConfig
+import com.ne.jp.shumipro_api.config.WebSecurityConfig
 import org.springframework.security.access.SecurityConfig
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer
 
 class SecurityInitializer :
-    AbstractSecurityWebApplicationInitializer(SecurityConfig::class.java, SessionConfig::class.java) {
+    AbstractSecurityWebApplicationInitializer(WebSecurityConfig::class.java, SessionConfig::class.java) {
 }
