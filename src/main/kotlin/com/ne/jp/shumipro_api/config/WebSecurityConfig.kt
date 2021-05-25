f022444267985ef26814dcac730262ab25e21325
Tom-Shumi <tshumiya281@gmail.com>
2021-05-25 23:42:04 +0900

    Add: jwt auth wip

--- src/main/kotlin/com/ne/jp/shumipro_api/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/ne/jp/shumipro_api/config/WebSecurityConfig.kt
@@ -1,9 +1,12 @@
 package com.ne.jp.shumipro_api.config
 
+import com.ne.jp.shumipro_api.mapper.UserMapper
 import com.ne.jp.shumipro_api.security.ApiPreAuthenticatedProcessingFilter
 import com.ne.jp.shumipro_api.security.ShumiproAccessDeniedHandler
 import com.ne.jp.shumipro_api.security.ShumiproAuthenticationEntryPoint
+import com.ne.jp.shumipro_api.security.ShumiproAuthenticationSuccessHandler
 import com.ne.jp.shumipro_api.service.ShumiproUserDetailsService
+import com.ne.jp.shumipro_api.service.TaskCommentService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -39,15 +42,40 @@ import org.springframework.security.web.authentication.preauth.PreAuthenticatedG
 import org.springframework.core.Ordered
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.filter.CorsFilter
+import org.springframework.web.filter.GenericFilterBean
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import org.springframework.security.web.authentication.AuthenticationFailureHandler
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler
+
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig: WebSecurityConfigurerAdapter() {
 
+    @Autowired
+    lateinit var userMapper: UserMapper
+
     @Value("\${front.origin}")
     private val frontOrigin: String = ""
+
+    @Value("\${security.secret-key}")
+    private val secretKey: String = ""
 
     @Bean
     fun passwordEncoder(): PasswordEncoder? {
@@ -63,15 +91,43 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
             .mvcMatchers("/api/task/**").hasRole("USER")
             .anyRequest().authenticated()
             .and()
-            .addFilter(preAuthenticatedProcessingFilter())
             .exceptionHandling()
                 .authenticationEntryPoint(authenticationEntryPoint())
                 .accessDeniedHandler(accessDeniedHandler())
             .and()
+            .formLogin()
+                .loginProcessingUrl("/login").permitAll()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler(authenticationSuccessHandler())
+                .failureHandler(authenticationFailureHandler())
+            .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessHandler(logoutSuccessHandler())
+            .and()
             .csrf().disable()
+            .addFilterBefore(tokenFilter(), UsernamePasswordAuthenticationFilter::class.java)
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
         http.cors().configurationSource(corsConfigurationSource())
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
+
+    @Autowired
+    @Throws(Exception::class)
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth.eraseCredentials(true)
+            .userDetailsService(shumiproUserDetailsService())
+            .passwordEncoder(passwordEncoder())
+    }
+
+    @Bean("ShumiproUserDetailsService")
+    fun shumiproUserDetailsService(): UserDetailsService? {
+        return ShumiproUserDetailsService(userMapper)
+    }
+
+    fun tokenFilter(): GenericFilterBean? {
+        return ShumiproTokenFilter(userMapper, secretKey)
     }
 
     @Override
@@ -101,31 +157,23 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
         return corsSource
     }
 
-    @Bean
-    fun authenticationUserDetailsService(): AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
-        return ShumiproUserDetailsService()
-    }
-
-    @Bean
-    fun preAuthenticationProvider(): PreAuthenticatedAuthenticationProvider {
-        val preAuthenticatedAuthenticationProvider = PreAuthenticatedAuthenticationProvider()
-        preAuthenticatedAuthenticationProvider.setPreAuthenticatedUserDetailsService(authenticationUserDetailsService())
-        preAuthenticatedAuthenticationProvider.setUserDetailsChecker(AccountStatusUserDetailsChecker())
-        return preAuthenticatedAuthenticationProvider
-    }
-
-    @Bean
-    fun preAuthenticatedProcessingFilter(): AbstractPreAuthenticatedProcessingFilter {
-        val preAuthenticatedProcessingFilter = ApiPreAuthenticatedProcessingFilter()
-        preAuthenticatedProcessingFilter.setAuthenticationManager(authenticationManager())
-        return preAuthenticatedProcessingFilter
-    }
-
     fun authenticationEntryPoint(): AuthenticationEntryPoint {
         return ShumiproAuthenticationEntryPoint()
     }
 
     fun accessDeniedHandler(): AccessDeniedHandler {
         return ShumiproAccessDeniedHandler()
+    }
+
+    fun authenticationSuccessHandler(): AuthenticationSuccessHandler? {
+        return ShumiproAuthenticationSuccessHandler(secretKey)
+    }
+
+    fun authenticationFailureHandler(): AuthenticationFailureHandler? {
+        return ShumiproAuthenticationFailureHandler()
+    }
+
+    fun logoutSuccessHandler(): LogoutSuccessHandler? {
+        return HttpStatusReturningLogoutSuccessHandler()
     }
 }
---
+++ src/main/kotlin/com/ne/jp/shumipro_api/security/ShumiproAuthenticationSuccessHandler.kt
@@ -0,0 +1,68 @@
+package com.ne.jp.shumipro_api.security
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.ne.jp.shumipro_api.entity.ShumiproLoginUser
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import java.util.*
+import java.util.concurrent.TimeUnit
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.Authentication
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import org.springframework.security.web.WebAttributes
+
+@Slf4j
+class ShumiproAuthenticationSuccessHandler: AuthenticationSuccessHandler {
+    companion object{
+        private val log = LoggerFactory.getLogger(ShumiproAuthenticationSuccessHandler::class.java)
+    }
+    private var algorithm: Algorithm? = null
+
+    constructor(secretKey: String?) {
+        Objects.requireNonNull(secretKey, "secret key must be not null")
+        algorithm = Algorithm.HMAC512(secretKey)
+    }
+
+    private val EXPIRATION_TIME = TimeUnit.MINUTES.toMillis(10L)
+
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        auth: Authentication
+    ) {
+        if (response.isCommitted()) {
+            log.info("Response has already been committed.")
+            return
+        }
+        setToken(response, generateToken(auth))
+        response.setStatus(HttpStatus.OK.value())
+        clearAuthenticationAttributes(request)
+    }
+
+    private fun generateToken(auth: Authentication): String {
+        val loginUser: ShumiproLoginUser = auth.principal as ShumiproLoginUser
+        val issuedAt = Date()
+        val notBefore = Date(issuedAt.time)
+        val expiresAt = Date(issuedAt.time + EXPIRATION_TIME)
+        val token: String = JWT.create()
+            .withIssuedAt(issuedAt)
+            .withNotBefore(notBefore)
+            .withExpiresAt(expiresAt)
+            .withSubject(loginUser.getUser().username)
+            .sign(algorithm)
+        log.debug("generate token : %s", token)
+        return token
+    }
+
+    private fun setToken(response: HttpServletResponse, token: String) {
+        response.setHeader("Authorization", String.format("Bearer %s", token))
+    }
+
+    private fun clearAuthenticationAttributes(request: HttpServletRequest) {
+        val session = request.getSession(false) ?: return
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION)
+    }
+}
