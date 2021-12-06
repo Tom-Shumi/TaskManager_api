3df5588ed055d639b7b5d8b201f1b5cc779b0968
Tom-Shumi <tshumiya281@gmail.com>
2021-12-06 22:45:51 +0900

    add Elasticsearch search process WIP

--- src/main/kotlin/com/ne/jp/shumipro_api/service/EsZeroSecondThinkingService.kt
+++ src/main/kotlin/com/ne/jp/shumipro_api/service/EsZeroSecondThinkingService.kt
@@ -1,12 +1,13 @@
 package com.ne.jp.shumipro_api.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.ne.jp.shumipro_api.Constants.Companion.ES_INDEX_NAME_CONTENT
 import com.ne.jp.shumipro_api.Constants.Companion.ES_INDEX_NAME_THEME
 import com.ne.jp.shumipro_api.dto.EsZeroSecondThinkingDocumentDto
-import com.ne.jp.shumipro_api.exception.ElasticsearchException
 import com.ne.jp.shumipro_api.repository.ElasticsearchClientRepository
 import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.index.query.BoolQueryBuilder
+import org.elasticsearch.index.query.MatchQueryBuilder
 import org.elasticsearch.index.query.TermQueryBuilder
 import org.elasticsearch.search.builder.SearchSourceBuilder
 import org.springframework.stereotype.Service
@@ -18,19 +19,25 @@ class EsZeroSecondThinkingService(private val elasticsearchClientRepository: Ela
     fun searchZeroSecondThinkingDocument(username: String, searchString: String): List<EsZeroSecondThinkingDocumentDto> {
         val searchSourceBuilder = SearchSourceBuilder()
         val boolQueryBuilder = BoolQueryBuilder()
+
         boolQueryBuilder.filter(TermQueryBuilder("username", username))
+                        .filter(MatchQueryBuilder("content", searchString))
 
         searchSourceBuilder.query(boolQueryBuilder)
 
-        val request = SearchRequest(ES_INDEX_NAME_THEME).source(searchSourceBuilder)
+        val themeList = search(ES_INDEX_NAME_THEME, searchSourceBuilder)
+        val contentList = search(ES_INDEX_NAME_CONTENT, searchSourceBuilder)
+
+        // TODO 返却するための処理
+        return listOf()
+    }
+
+    private fun search(index: String, searchSourceBuilder: SearchSourceBuilder): List<EsZeroSecondThinkingDocumentDto> {
+        val request = SearchRequest(index).source(searchSourceBuilder)
 
         val response = elasticsearchClientRepository.search(request)
         val searchHit = response.hits.hits
 
-        for (hit in searchHit) {
-            val document = objectMapper.convertValue(hit.sourceAsMap, EsZeroSecondThinkingDocumentDto::class.java)
-            println(document)
-        }
-        return listOf()
+        return searchHit.map{ hit -> objectMapper.convertValue(hit.sourceAsMap, EsZeroSecondThinkingDocumentDto::class.java)}.toList()
     }
 }
