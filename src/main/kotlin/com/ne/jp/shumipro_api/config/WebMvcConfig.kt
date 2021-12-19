95e1da5b7a36e0401e8170c0f63561118bff73fc
Tom-Shumi <tshumiya281@gmail.com>
2021-12-19 10:37:34 +0900

    mod cors config

--- src/main/kotlin/com/ne/jp/shumipro_api/config/WebMvcConfig.kt
+++ src/main/kotlin/com/ne/jp/shumipro_api/config/WebMvcConfig.kt
@@ -1,7 +1,11 @@
 package com.ne.jp.shumipro_api.config
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import java.util.TimeZone
@@ -15,12 +19,19 @@ class WebMvcConfig: WebMvcConfigurer {
     @Value("\${allow.front.origin}")
     private val frontOrigin: String = ""
 
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins(frontOrigin)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowCredentials(true);
-
-        super.addCorsMappings(registry)
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true;
+        config.addAllowedOrigin(frontOrigin);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return CorsFilter(source);
     }
 }
